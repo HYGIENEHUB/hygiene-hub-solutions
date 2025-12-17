829cdb7e6cb16559362bab20843f7536487cd6e9
HYGIENEHUB <hygienhub@gmail.com>
2025-12-18 01:29:27 +0200

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: "HYGIENE HUB SOLUTIONS",
+  description: "Elevating Hygiene Standards with Confidence"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
