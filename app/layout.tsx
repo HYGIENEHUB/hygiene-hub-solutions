3c8fd7475eb8d9469a6e2f139dbbe3aafda4ee8a
HYGIENEHUB <hygienhub@gmail.com>
2025-12-18 01:05:20 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
+import type { ReactNode } from "react";
 
 export const metadata = {
   title: "HYGIENE HUB SOLUTIONS",
-    description: "Elevating Hygiene Standards with Confidence"
-  };
+  description: "Elevating Hygiene Standards with Confidence",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-         <html lang="en">
-             <body>{children}</body>
-            </html>
-       );
-  }
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
